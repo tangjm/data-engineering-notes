5e1cd0519ebe74f4cac0fe5d64b1c00ac56f289c
tangjm <jaredtang@hotmail.co.uk>
2022-04-28 16:57:34 +0100

    add homepage, page directory for react components, and public directory for static assets

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// This is the page that is rendered when users visit the root '/' of your application
+
+function HomePage() {
+  return <div>Welcome to Next.js! 
+    <p>How are you doing Today?</p>
+  </div>
+}
+
+export default HomePage
