e35ca80a7d3bea979b6cf220ecce1fc93b989043
tangjm <jaredtang@hotmail.co.uk>
2022-06-20 10:04:20 +0100

    Update aws.js to include AWS module headers

--- pages/aws.js
+++ pages/aws.js
@@ -13,6 +13,20 @@ function createLinksNotes(subdir, notes) {
   })
 }
 
+const moduleNames = [
+  null,
+  "Module 1: The Cloud",
+  "Module 2: Economics",
+  "Module 3: AWS Infrastructure",
+  "Module 4: Cloud security",
+  "Module 5: Networking",
+  "Module 6: Compute Services",
+  "Module 7: Storage services",
+  "Module 8: Database services",
+  "Module 9: Cloud Architecture",
+  "Module 10: Auto scaling and Monitoring",
+]
+
 const subdirs = {
   "module1": "module1_cloud_concepts",
   "module2": "module2_cloud_economics_billing",
@@ -38,6 +52,7 @@ export async function getStaticProps() {
   let fileArr = moduleArr.map(n => {
     let subdir = subdirs["module" + n]
     return {
+      "id": n,
       "subdir": subdir,
       "fileHTML": getNotes(...["AWS", subdir])
     }
@@ -67,50 +82,18 @@ export default function awsNotes(props) {
           AWS Notes
         </h1>
         <h2>AWS Academy Cloud Foundations</h2>
-        <p className={styles.description}>
-          * [Module 1: The Cloud](/AWS/module1_cloud_concepts/notes_on_cloud_concepts)
-          * [Module 2: Economics](/AWS/module2_cloud_economics_billing/notes_pricing)
-          * [Module 3: AWS Infrastructure](/AWS/module3_global_infrastructure/notes_global_infrastructure)
-          * [Module 4: Cloud security](/AWS/module4_cloud_security/notes_cloud_security)
+        {/* <div className={styles.description}> */}
+        <div>
+          {props.modules.map(module => {
 
-          Module 5: Networking
-
-          * [Networking](/AWS/module5_networking/notes_networking.md)
-          * [CLoudFront](/AWS/module5_networking/notes_cloudfront.md)
-          * [Route53](/AWS/module5_networking/notes_route53.md)
-
-          Module 6: Compute Services
-
-          * [EC2](/AWS/module6_compute/notes_compute.md)
-          * [Containers](/AWS/module6_compute/notes_containers.md)
-          * [Elastic Beanstalk](/AWS/module6_compute/notes_elastic_beanstalk.md)
-          * [AWS Lambda](/AWS/module6_compute/notes_lambda.md)
-
-          Module 7: Storage services
-
-          * [S3](/AWS/module7_storage/notes_s3.md)
-          * [Elastic Block Store (EBS)](/AWS/module7_storage/notes_ebs.md)
-          * [Elastic File System (EFS)](/AWS/module7_storage/notes_efs.md)
-          * [S3 Glacier](/AWS/module7_storage/notes_s3_glacier.md)
-
-          Module 8: Database services
-
-          * [RDS](/AWS/module8_database/amazon_rds.md)
-          * [Aurora](/AWS/module8_database/aurora.md)
-          * [DynamoDB](/AWS/module8_database/dynamo_db.md)
-          * [Redshift](/AWS/module8_database/redshift.md)
-          * [How to choose?](/AWS/module8_database/selecting_a_database_service.md)
-
-          Module 9: Cloud Architecture
-
-          * [AWS Well-Architected Framework](/AWS/module9_cloud_architecture/notes_cloud_architecture.md)
-          * [High availability](/AWS/module9_cloud_architecture/high_availability.md)
-
-          Module 10: Auto scaling and Monitoring
-        </p>
-        {props.modules.map(module => {
-          return createLinksNotes(module.subdir, module.fileHTML)
-        })}
+            return <>
+              <h2>{moduleNames[module.id]}</h2>
+              <ul>
+                {createLinksNotes(module.subdir, module.fileHTML)}
+              </ul>
+            </>
+          })}
+        </div>
       </main>
 
       <footer className={styles.footer}>
